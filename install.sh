6db740b2f3f8bbbcbcd39357233bd9491c2df1fb
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2023-01-23 09:50:38 -0300

    install: add nvim install

--- install.sh
+++ install.sh
@@ -3,11 +3,18 @@ mv ~/.zshrc ~/.zshrc_backup 2> /dev/null
 echo ". $PWD/zshrc" >> ~/.zshrc
 touch zshrc.local
 
-# install vundle
+
+# vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-# .vimrc
 echo "source $PWD/vimrc" >> ~/.vimrc
 
-# Copy .gitconfig
+
+# nvimrc
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+echo "source $PWD/nvimrc" >> ~/.config/nvim/init.vim
+pip install pynvim
+
+
+# .gitconfig
 mv ~/.gitconfig ~/.gitconfig_backup 2> /dev/null
 cp gitconfig ~/.gitconfig
