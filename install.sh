bb1a5c0fe0ba4fefcf4701c7d07afff56a7f8712
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2023-03-02 18:46:46 -0300

    Add LunarVim installation

--- install.sh
+++ install.sh
@@ -87,3 +87,8 @@ pip install pynvim
 # .gitconfig
 mv ~/.gitconfig ~/.gitconfig_backup 2> /dev/null
 cp gitconfig ~/.gitconfig
+
+
+# LunarVim
+curl https://sh.rustup.rs -sSf | sh   # Rust
+LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/fc6873809934917b470bff1b072171879899a36b/utils/installer/install.sh)
