c4731fc34582a48c40acd0e8430edf4b883e1378
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2023-03-02 19:11:34 -0300

    lvim: Add LunarVim config

---
+++ lvim.lua
@@ -0,0 +1,64 @@
+vim.opt.relativenumber = true
+
+lvim.log.level = "warn"
+lvim.format_on_save.enabled = false
+lvim.colorscheme = "lunar"
+
+lvim.leader = "space"
+lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+
+lvim.builtin.alpha.active = true
+lvim.builtin.alpha.mode = "dashboard"
+lvim.builtin.terminal.active = true
+lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+
+lvim.builtin.treesitter.ensure_installed = {
+  "bash",
+  "c",
+  "javascript",
+  "json",
+  "lua",
+  "python",
+  "typescript",
+  "tsx",
+  "css",
+  "rust",
+  "java",
+  "yaml",
+}
+
+lvim.builtin.treesitter.ignore_install = { "haskell" }
+lvim.builtin.treesitter.highlight.enable = true
+
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
+
+-- Additional Plugins
+lvim.plugins = {
+  {"vim-test/vim-test"},
+  {"zbirenbaum/copilot.lua",
+    event = { "VimEnter" },
+    config = function()
+      vim.defer_fn(function()
+        require("copilot").setup {
+          plugin_manager_path = get_runtime_dir() .. "/site/pack/packer",
+          filetypes = {
+            ["*"] = true,
+          }
+        }
+        end, 100)
+    end,
+  },
+  {"zbirenbaum/copilot-cmp",
+    after = { "copilot.lua", "nvim-cmp" },
+    config = function ()
+      require("copilot_cmp").setup()
+    end
+  },
+}
+
+lvim.builtin.cmp.formatting.source_names["copilot"] = "(Copilot)"
+table.insert(lvim.builtin.cmp.sources, 1, { name = "copilot" })
+
+lvim.keys.normal_mode["<leader>T"] = ":TestFile<cr>"
+lvim.keys.normal_mode["<leader>g"] = ":Telescope live_grep<cr>"
