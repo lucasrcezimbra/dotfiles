e97b9667272a655ddab95a277cd4190c500d935a
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2023-10-23 09:36:21 -0300

    Update pyenv installation

--- install.sh
+++ install.sh
@@ -44,7 +44,7 @@ sudo apt-get autoclean -y
 
 
 # Install pyenv
-sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils
+sudo apt-get install -y build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
 
 
