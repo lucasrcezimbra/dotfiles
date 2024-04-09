f125192a7088dde877b606c070ac06a1ab5c16f1
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2024-04-09 18:41:31 -0300

    feat(lvim): packer to lazy

--- lvim.lua
+++ lvim.lua
@@ -57,8 +57,8 @@ lvim.plugins = {
     end,
   },
   {"iamcco/markdown-preview.nvim",
-    run = "cd app && npm install",
-    setup = function() vim.g.mkdp_filetypes = { "markdown" } end,
+    build = "cd app && npm install",
+    init = function() vim.g.mkdp_filetypes = { "markdown" } end,
     ft = { "markdown" },
   },
 
