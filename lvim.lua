f8ca7b7ebfe01e846a06ada5eff9acdcb96b50a8
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2024-07-05 12:49:27 -0300

    style: add StyLua as pre-commit

--- lvim.lua
+++ lvim.lua
@@ -14,15 +14,15 @@ lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 
 lvim.builtin.treesitter.ensure_installed = {
-  "bash",
-  "javascript",
-  "json",
-  "lua",
-  "python",
-  "typescript",
-  "tsx",
-  "css",
-  "yaml",
+    "bash",
+    "javascript",
+    "json",
+    "lua",
+    "python",
+    "typescript",
+    "tsx",
+    "css",
+    "yaml",
 }
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
@@ -31,76 +31,85 @@ lvim.builtin.treesitter.highlight.enable = true
 -- https://www.lunarvim.org/docs/configuration/language-features/language-servers#by-overriding-the-setup
 vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
 require("lvim.lsp.manager").setup("pyright", {
-  settings = {
-    python = { analysis = { typeCheckingMode = "off" } },
-  },
+    settings = {
+        python = { analysis = { typeCheckingMode = "off" } },
+    },
 })
 
 -- Additional Plugins
 lvim.plugins = {
-  {"mfussenegger/nvim-dap-python",
-    config = function()
-      dap_python = require("dap-python")
-      dap_python.setup("~/.pyenv/versions/debugpy/bin/python")
-      dap_python.test_runner = 'pytest'
-    end,
-  },
-  {"supermaven-inc/supermaven-nvim",
-    config = function()
-      require("supermaven-nvim").setup({
-        keymaps = {
-            accept_suggestion = "<M-l>",
-            accept_word = "<M-a>",
-          },
-      })
-    end,
-  },
-  {"iamcco/markdown-preview.nvim",
-    build = "cd app && npm install",
-    init = function() vim.g.mkdp_filetypes = { "markdown" } end,
-    ft = { "markdown" },
-  },
-  {"nvim-neotest/neotest",
-    config = function ()
-      require("neotest").setup({
-        adapters = {
-          require("neotest-python")({
-            dap = { justMyCode = false },
-            args = {"-vv"},
-          })
-        }
-      })
-    end,
-    dependencies = {
-      "nvim-neotest/neotest-python",
-      "nvim-neotest/nvim-nio",
-      "nvim-lua/plenary.nvim",
-      "antoinemadec/FixCursorHold.nvim",
-      "nvim-treesitter/nvim-treesitter",
+    {
+        "mfussenegger/nvim-dap-python",
+        config = function()
+            dap_python = require("dap-python")
+            dap_python.setup("~/.pyenv/versions/debugpy/bin/python")
+            dap_python.test_runner = "pytest"
+        end,
     },
-  },
-  {"tiagovla/scope.nvim",
-    config = function()
-      require("scope").setup()
-    end
-  },
-  {'ggandor/leap.nvim',
-    config = function()
-      require('leap').create_default_mappings()
-    end
-  },
+    {
+        "supermaven-inc/supermaven-nvim",
+        config = function()
+            require("supermaven-nvim").setup({
+                keymaps = {
+                    accept_suggestion = "<M-l>",
+                    accept_word = "<M-a>",
+                },
+            })
+        end,
+    },
+    {
+        "iamcco/markdown-preview.nvim",
+        build = "cd app && npm install",
+        init = function()
+            vim.g.mkdp_filetypes = { "markdown" }
+        end,
+        ft = { "markdown" },
+    },
+    {
+        "nvim-neotest/neotest",
+        config = function()
+            require("neotest").setup({
+                adapters = {
+                    require("neotest-python")({
+                        dap = { justMyCode = false },
+                        args = { "-vv" },
+                    }),
+                },
+            })
+        end,
+        dependencies = {
+            "nvim-neotest/neotest-python",
+            "nvim-neotest/nvim-nio",
+            "nvim-lua/plenary.nvim",
+            "antoinemadec/FixCursorHold.nvim",
+            "nvim-treesitter/nvim-treesitter",
+        },
+    },
+    {
+        "tiagovla/scope.nvim",
+        config = function()
+            require("scope").setup()
+        end,
+    },
+    {
+        "ggandor/leap.nvim",
+        config = function()
+            require("leap").create_default_mappings()
+        end,
+    },
 }
 lvim.keys.normal_mode["<leader>g"] = ":Telescope live_grep<cr>"
 
-lvim.builtin.which_key.mappings["dm"] = {":lua require('neotest').run.run({strategy = 'dap'})<CR>", "Neotest Closest"}
-lvim.builtin.which_key.mappings["df"] = {":lua require('neotest').run.run({vim.fn.expand('%'), strategy = 'dap'})<CR>", "Neotest File"}
+lvim.builtin.which_key.mappings["dm"] = { ":lua require('neotest').run.run({strategy = 'dap'})<CR>", "Neotest Closest" }
+lvim.builtin.which_key.mappings["df"] =
+    { ":lua require('neotest').run.run({vim.fn.expand('%'), strategy = 'dap'})<CR>", "Neotest File" }
 
 lvim.builtin.which_key.mappings["t"] = "+Test"
-lvim.builtin.which_key.mappings["ta"] = {":lua require('neotest').run.run(vim.fn.getcwd())<CR>", "Run All"}
-lvim.builtin.which_key.mappings["tf"] = {":lua require('neotest').run.run(vim.fn.expand('%'))<CR>", "Run File"}
-lvim.builtin.which_key.mappings["tm"] = {":lua require('neotest').run.run()<CR>", "Run Closest"}
-lvim.builtin.which_key.mappings["to"] = {":Neotest output<CR>", "Output"}
-lvim.builtin.which_key.mappings["ts"] = {":lua require('neotest').summary.toggle()<cr>", "Summary"}
+lvim.builtin.which_key.mappings["ta"] = { ":lua require('neotest').run.run(vim.fn.getcwd())<CR>", "Run All" }
+lvim.builtin.which_key.mappings["tf"] = { ":lua require('neotest').run.run(vim.fn.expand('%'))<CR>", "Run File" }
+lvim.builtin.which_key.mappings["tm"] = { ":lua require('neotest').run.run()<CR>", "Run Closest" }
+lvim.builtin.which_key.mappings["to"] = { ":Neotest output<CR>", "Output" }
+lvim.builtin.which_key.mappings["ts"] = { ":lua require('neotest').summary.toggle()<cr>", "Summary" }
 
 lvim.keys.normal_mode["<C-t>"] = ":tabnew<CR>"
 lvim.keys.normal_mode["gt"] = ":tabnext<CR>"
