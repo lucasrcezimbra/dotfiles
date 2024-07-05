f661bf4f61e66d02fe253e12ff2881217b154a2b
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2024-07-05 14:45:38 -0300

    feat(lvim): trying nvim-bqf

--- lvim.lua
+++ lvim.lua
@@ -97,6 +97,32 @@ lvim.plugins = {
             require("leap").create_default_mappings()
         end,
     },
+    {
+        "kevinhwang91/nvim-bqf",
+        event = { "BufRead", "BufNew" },
+        config = function()
+            require("bqf").setup({
+                auto_enable = true,
+                preview = {
+                    win_height = 12,
+                    win_vheight = 12,
+                    delay_syntax = 80,
+                    border_chars = { "┃", "┃", "━", "━", "┏", "┓", "┗", "┛", "█" },
+                },
+                func_map = {
+                    vsplit = "",
+                    ptogglemode = "z,",
+                    stoggleup = "",
+                },
+                filter = {
+                    fzf = {
+                        action_for = { ["ctrl-s"] = "split" },
+                        extra_opts = { "--bind", "ctrl-o:toggle-all", "--prompt", "> " },
+                    },
+                },
+            })
+        end,
+    },
 }
 lvim.keys.normal_mode["<leader>g"] = ":Telescope live_grep<cr>"
 
