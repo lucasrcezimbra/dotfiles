6773cbc8f23600c595ddb0964a25b5497564a3e9
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2025-04-05 16:12:02 -0300

    feat(nvim): enable extra LazyVim plugins

--- nvim/config/lazy.lua
+++ nvim/config/lazy.lua
@@ -18,6 +18,10 @@ require("lazy").setup({
     spec = {
         -- add LazyVim and import its plugins
         { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+        { import = "lazyvim.plugins.extras.ai.copilot" },
+        { import = "lazyvim.plugins.extras.coding.yanky" },
+        { import = "lazyvim.plugins.extras.lang.markdown" },
+        { import = "lazyvim.plugins.extras.lang.python" },
         -- import/override with your plugins
         { import = "plugins" },
     },
