1e0a4f1e3c4122be81dad5db086ddf3d1fbb1abd
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2025-04-05 16:09:15 -0300

    chore: drop nvimrc

--- install.sh
+++ install.sh
@@ -60,11 +60,6 @@ mv ~/.zshrc ~/.zshrc_backup 2> /dev/null
 echo ". $PWD/zshrc" >> ~/.zshrc
 touch zshrc.local
 
-# nvimrc
-curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-echo "source $PWD/nvimrc" >> ~/.config/nvim/init.vim
-pip install pynvim
-
 # .gitconfig
 mv ~/.gitconfig ~/.gitconfig_backup 2> /dev/null
 ln -s $PWD/gitconfig ~/.gitconfig
