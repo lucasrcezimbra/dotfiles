811abe33d609743d38061a6261a956ecbcec1a5e
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2025-04-05 10:29:19 -0300

    chore: drop vimrc

--- install.sh
+++ install.sh
@@ -60,10 +60,6 @@ mv ~/.zshrc ~/.zshrc_backup 2> /dev/null
 echo ". $PWD/zshrc" >> ~/.zshrc
 touch zshrc.local
 
-# vimrc
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-echo "source $PWD/vimrc" >> ~/.vimrc
-
 # nvimrc
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 echo "source $PWD/nvimrc" >> ~/.config/nvim/init.vim
