9692096139801d4b45bc080a0074605c7c4325a7
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2025-05-19 18:11:20 -0300

    feat(nvim): markdown plugins (break-line-at-80 and url2markdown)

---
+++ nvim/plugins/break-at-80.lua
@@ -0,0 +1,32 @@
+local function break_line_at_80()
+    local bufnr = vim.api.nvim_get_current_buf()
+    local cursor_pos = vim.api.nvim_win_get_cursor(0)
+    local line_num = cursor_pos[1]
+    local cur_line = vim.api.nvim_buf_get_lines(bufnr, line_num - 1, line_num, false)[1]
+    local break_pos = 80
+
+    -- If the line is shorter than the break position, do nothing
+    if #cur_line <= break_pos then
+        return
+    end
+
+    -- Find the position of the last whitespace before the break position
+    local break_at = cur_line:sub(1, break_pos):reverse():find("%s")
+    if break_at then
+        break_at = break_pos - break_at + 1
+    else
+        break_at = break_pos
+    end
+
+    -- Split the line
+    local new_cur_line = cur_line:sub(1, break_at):gsub("%s+$", "")
+    local new_next_line = "  " .. cur_line:sub(break_at + 1):gsub("^%s+", "")
+
+    -- Update the buffer
+    vim.api.nvim_buf_set_lines(bufnr, line_num - 1, line_num, false, { new_cur_line, new_next_line })
+    vim.api.nvim_win_set_cursor(0, { line_num + 1, 0 })
+end
+
+vim.keymap.set("n", "<leader>cb", break_line_at_80, { desc = "Break line at 80ish" })
+
+return {}
---
+++ nvim/plugins/url2markdown.lua
@@ -0,0 +1,40 @@
+local curl = require("plenary.curl")
+
+local function get_cursor_url()
+    local url = vim.fn.expand("<cWORD>")
+    local cleaned_url = url:match("^(.-)[%p%s]*$")
+    return cleaned_url
+end
+
+local function get_html_content(url)
+    return curl.get(url).body
+end
+
+local function extract_title(html)
+    local title = html:match("<title>(.-)</title>")
+    return title and title:gsub("%s+", " ") or ""
+end
+
+local function replace_url_with_markdown()
+    local url = get_cursor_url()
+    if url:match("^https?://") then
+        local html_content = get_html_content(url)
+        if html_content then
+            local title = extract_title(html_content)
+            if title and #title > 0 then
+                local markdown = string.format("[%s](%s)", title, url)
+                vim.cmd("normal! ciW" .. markdown)
+            else
+                print("Failed to extract title from the URL")
+            end
+        else
+            print("Failed to fetch content from the URL")
+        end
+    else
+        print("Cursor is not at a valid URL")
+    end
+end
+
+vim.keymap.set("n", "<leader>cu", replace_url_with_markdown, { desc = "URL to Markdown" })
+
+return {}
