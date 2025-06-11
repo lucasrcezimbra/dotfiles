5d0ff8cf1e1c7b5b36026aa218ae68855b40ad54
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2025-06-11 17:16:27 -0300

    feat(nvim): <leader>mt to format tables and <leader>p to paste without yanking

--- nvim/config/keymaps.lua
+++ nvim/config/keymaps.lua
@@ -2,3 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("n", "<S-TAB>", ":bnext<CR>")
+
+vim.keymap.set("v", "<leader>mt", ":!column -t -s '|' -o '|'<CR>", { desc = "Format table" })
+
+vim.keymap.set("v", "<leader>p", [["_dP]], { desc = "Paste without yanking" })
