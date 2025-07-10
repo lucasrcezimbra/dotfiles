2b76f210474b25eb66da88e624953ea7229f2af0
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2025-07-10 15:41:27 -0300

    feat(nvim): change colorscheme to github_dark

---
+++ nvim/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+    { "projekt0n/github-nvim-theme" },
+    -- { "rebelot/kanagawa.nvim" },
+    -- { "EdenEast/nightfox.nvim" },
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "github_dark",
+            -- colorscheme = "kanagawa",
+            -- colorscheme = "nightfox",
+        },
+    },
+}
