62912985a59d698cfb7f67faf4cadf264f691237
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2025-09-13 11:27:17 -0300

    feat(omarchy-install): mise tools

--- omarchy.install.sh
+++ omarchy.install.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+# mise tools
+mv ~/.config/mise/config.toml{,.backup}
+ln -s "$PWD/mise.toml" ~/.config/mise/config.toml
+touch mise.local.toml
+ln -s "$PWD/mise.local.toml" ~/.config/mise/config.local.toml
+mise install
+
 # git
 mv ~/.gitconfig ~/.gitconfig_backup 2>/dev/null
 ln -s "$PWD/gitconfig" ~/.gitconfig
