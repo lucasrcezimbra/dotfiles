b88ca60413c736f8165026fa0b42275f50295e07
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2025-09-13 10:58:04 -0300

    chore(omarchy-install): GitHub CLI auth and dotfiles using SSH

--- omarchy.install.sh
+++ omarchy.install.sh
@@ -9,3 +9,11 @@ ln -s "$PWD/gitconfig" ~/.gitconfig
 # hyprland
 mv ~/.config/hypr/ ~/.config/hypr_backup/ 2>/dev/null
 ln -s "$PWD/hypr/" ~/.config/
+
+# GitHub CLI
+gh auth login
+
+# self config
+git remote set-url origin git@github.com:lucasrcezimbra/dotfiles.git
+# TODO: remove XFCE stuff from pre-commit
+# pre-commit install
